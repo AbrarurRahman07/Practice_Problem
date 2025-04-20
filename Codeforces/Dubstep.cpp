560b502728feb43c94397145054c050f2df1c9e0
Abrar Tahsin <abrarur.cse@gmail.com>
2025-04-21 02:05:47 +0600

    Update Dubstep.cpp

--- Codeforces/Dubstep.cpp
+++ Codeforces/Dubstep.cpp
@@ -1,7 +1,27 @@
 #include<iostream>
-#include<string>
+#include<string.h>
 using namespace std;
 int main()
 {
-
+    string s,t;
+    cin>>s;
+    int count=0;
+    for(int i=0; i<s.size(); i++)
+    {
+        if(s[i]=='W'&&s[i+1]=='U'&&s[i+2]=='B')
+        {
+            if(count!=0)
+            {
+                t+=" ";
+                count=0;
+            }
+            i=i+2;
+        }
+        else
+        {
+            t+=s[i];
+            count++;
+        }
+    }
+    cout<<t<<endl;
 }
